a47625c2a3a0c85d9888133f6f848f98568b4c8c
vageesh-singh-iphtech <vageesh.singh@iphtechnologies.com>
2025-02-18 15:54:14 +0530

    Initial Commit

---
+++ UI-Project-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI-Project-6
+//
+//  Created by iPHTech 35 on 18/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
